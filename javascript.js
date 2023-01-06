7175feee986ed72f2a077402bdc0c2423e004bff
DahirAyoub <Dahirayoub00@gmail.com>
2023-01-06 00:21:35 -0500

    Rough Copy of game

--- javascript.js
+++ javascript.js
@@ -4,53 +4,117 @@ function getComputerChoice(){
     return options[choice]
 }
 
-function playerSelection(){
-    let choice = prompt("Rock, Paper, Scissors?").toLowerCase()
-    return choice
-}
-
-function simulator(playerSelection,computerSelection){
-    let outcome;
-    switch(playerSelection,computerSelection){
-        case playerSelection=="rock" && computerSelection=="rock":
-            outcome="Tie!"
-            break;
-        
-        case playerSelection=="rock" && computerSelection=="paper":
-            outcome="Lose"
-            break;
-        
-        case playerSelection=="rock" && computerSelection=="scissors":
-            outcome="Win"
-            break;
-        
-        case playerSelection=="paper" && computerSelection=="paper":
-            outcome="Tie!"
-            break;
-            
-        case playerSelection=="paper" && computerSelection=="scissors":
-            outcome="Lose"
-            break;
-            
-        case playerSelection=="paper" && computerSelection=="rock":
-            outcome="Win"
-            break;
-
-        case playerSelection=="scissors" && computerSelection=="scissors":
-            outcome="Tie!"
-            break;
-                
-        case playerSelection=="scissors" && computerSelection=="rock":
-            outcome="Lose"
-            break;
-                
-        case playerSelection=="scissors" && computerSelection=="paper":
-            outcome="Win"
-            break;
+function simulator(playerSelection){
+    let computerSelection = getComputerChoice();
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        const displayedOutcome=document.querySelector('#Outcome')
+        displayedOutcome.textContent="IT'S A TIE!"
+        return "Tie!";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        const displayedOutcome=document.querySelector('#Outcome')
+        displayedOutcome.textContent="YOU LOST!"
+        return "Lose";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        const displayedOutcome=document.querySelector('#Outcome')
+        displayedOutcome.textContent="YOU WON!"
+        return "Win";
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        const displayedOutcome=document.querySelector('#Outcome')
+        displayedOutcome.textContent="IT'S A TIE!"
+        return "Tie!";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        const displayedOutcome=document.querySelector('#Outcome')
+        displayedOutcome.textContent="YOU LOST!"
+        return "Lose";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        const displayedOutcome=document.querySelector('#Outcome')
+        displayedOutcome.textContent="YOU WON!"
+        return "Win";
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        const displayedOutcome=document.querySelector('#Outcome')
+        displayedOutcome.textContent="IT'S A TIE!"
+        return "Tie!";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        const displayedOutcome=document.querySelector('#Outcome')
+        displayedOutcome.textContent="YOU LOST!"
+        return "Lose";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        const displayedOutcome=document.querySelector('#Outcome')
+        displayedOutcome.textContent="YOU WON!"
+        return "Win";
     }
-    return outcome
 }
 
-for (let i=0; i<5; i++){
-    console.log(simulator(playerSelection(),computerSelection()))
+function playerScoreCalculator(outcome){
+    var x=0
+    const playerScoreDom=document.querySelector('#playerscore')
+    if(outcome="Win"){
+        x++
+        playerScoreDom.textContent='Player:'+ x
+        
+    }
+    else{
+        return x
+    }
+}  
+
+
+
+function computerScoreCalculator(outcome){
+    var x=0
+    if(outcome="Win"){
+        return x++
+    }
+    else{
+        return x
+    }
+}  
+
+
+
+const rockChoice = document.querySelector('#rockimg')
+const paperChoice=document.querySelector('#paperimg')
+const scissorsChoice=document.querySelector('#scissorsimg')
+
+let choice;
+
+
+
+if(rockChoice){
+    rockChoice.addEventListener("click", e => {
+        choice='rock'
+        let outcome = simulator(choice);
+        console.log(outcome);
+        // playerScoreCalculator(outcome)
+        // const playerScoreDom=document.querySelector('#playerscore')
+        // playerScoreDom.textContent='Player:'+playerScoreCalculator(outcome)
+
+        const computerScoreDom=document.querySelector('#computerscore')
+        computerScoreDom.textContent='Computer:'+computerScoreCalculator(outcome)
+
+        // const displayedOutcome=document.querySelector('#Outcome')
+        // displayedOutcome.textContent="YOU WON!"
+
+    })
 }
+
+if(paperChoice){
+    paperChoice.addEventListener("click",e=>{
+        choice='paper'
+        let outcome = simulator(choice);
+        console.log(outcome);  
+    })
+}
+
+if(scissorsChoice){
+    scissorsChoice.addEventListener("click",e=>{
+        choice='scissors'
+        let outcome = simulator(choice);
+        console.log(outcome);
+    })
+}
+
+
+
+
+
